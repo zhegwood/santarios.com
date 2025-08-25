9458883e826ab7748887cad3a656b7163f1253b8
Zach <zhegwood@gmail.com>
2025-08-25 12:36:36 -0600

    updates to venues

--- src/components/PastVenue.vue
+++ src/components/PastVenue.vue
@@ -11,16 +11,14 @@ const props = withDefaults(defineProps<Props>(), {
   <div>
     <h3 v-if="!props.hideTitle">Past Venue Highlights</h3>
     <ul>
+      <li>Colorado State Fair - Pueblo, CO</li>
       <li>Cheyenne Mountain Resort - Colorado Springs, CO</li>
-      <li>Little Cambodia - Salida, CO</li>
       <li>Four Mile Historic Park - Denver, CO</li>
       <li>Mesa Theater - Grand Junction, CO</li>
-      <li>A Church - Salida, CO</li>
-      <li>Victoria Tavern - Salida, CO</li>
       <li>Boulder Theater - Boulder, CO</li>
       <li>Stargazers - Colorado Springs, CO</li>
       <li>Nissi's - Lafayette, CO</li>
-      <li>Steamplant - Salida, CO</li>
+      <li>SteamPlant Events Center - Salida, CO</li>
       <li>Tabor Opera House - Leadville, CO</li>
       <li>The Lariat - Buena Vista, CO</li>
       <li>Independent Whitewater - Salida, CO</li>
